c1d46fcb43fc63c2e3e19e0c7d6bc96ce29ff376
Captain-Toasty <47210819+Captain-Toasty@users.noreply.github.com>
2021-10-03 16:45:14 +0100

    Work on user sign in

---
+++ src/main/java/net/javasguides/registration/controller/CustomerServlet.java
@@ -0,0 +1,62 @@
+package net.javasguides.registration.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.javasguides.registration.dao.CustomerDao;
+import net.javasguides.registration.model.Customer;
+
+/**
+ * Servlet implementation class customerServlet
+ */
+@WebServlet("/register")
+public class CustomerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private CustomerDao customerDao = new CustomerDao();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CustomerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/customerRegister.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		
+		try {
+
+			customerDao.registerCustomer(new Customer(firstName, lastName, password, email));
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/customerDetails.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
--- src/main/java/net/javasguides/registration/dao/CustomerDao.java
+++ src/main/java/net/javasguides/registration/dao/CustomerDao.java
@@ -1,38 +1,44 @@
-package net.gym.login.dao;
+package net.javasguides.registration.dao;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import net.gym.login.model.Login;
+import net.javasguides.registration.model.Customer;
 
-public class LoginDao {
+public class CustomerDao {
 
-    public boolean validate(Login login) throws ClassNotFoundException {
-        boolean status = false;
+    public int registerCustomer(Customer customer) throws ClassNotFoundException {
+        String INSERT_USERS_SQL = "INSERT INTO customer" +
+            "  (id, first_name, last_name, password, email) VALUES " +
+            " (?, ?, ?, ?, ?);";
+
+        int result = 0;
 
         Class.forName("com.mysql.jdbc.Driver");
 
         try (Connection connection = DriverManager
-            .getConnection("jdbc:mysql://localhost:3306/gym?useSSL=false", "root", "admin");
+            .getConnection("jdbc:mysql://localhost:3306/mysql_database?allowPublicKeyRetrieval=true&useSSL=false & serverTimezone=UTC", "root", "geochelone");
 
             // Step 2:Create a statement using connection object
-            PreparedStatement preparedStatement = connection
-            .prepareStatement("select * from login where username = ? and password = ? ")) {
-            preparedStatement.setString(1, login.getUsername());
-            preparedStatement.setString(2, login.getPassword());
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, customer.getFirstName());
+            preparedStatement.setString(3, customer.getLastName());
+            preparedStatement.setString(4, customer.getPassword());
+            preparedStatement.setString(5, customer.getEmail());
 
             System.out.println(preparedStatement);
-            ResultSet rs = preparedStatement.executeQuery();
-            status = rs.next();
+            // Step 3: Execute the query or update query
+            result = preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
             // process sql exception
             printSQLException(e);
         }
-        return status;
+        return result;
     }
 
     private void printSQLException(SQLException ex) {
