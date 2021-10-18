07c332d006a00f9c1d6d9d27b546e08637d9aaaf
thiago <thiagosteps@gmail.com>
2021-10-18 19:52:12 +0100

    unit tests

---
+++ src/main/java/net/gym/classes/dao/TestClassesCRUDDao.java
@@ -0,0 +1,16 @@
+package net.gym.classes.dao;
+
+import org.junit.jupiter.api.Test;
+
+import net.gym.classes.model.Classes;
+
+class TestClassesCRUDDao {
+
+	@Test
+	void test() {
+
+		Classes classes = new Classes(0, "soccer", "ronaldo", "13:00");
+
+	}
+
+}
